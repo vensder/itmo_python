8d52791b59385612dc8e6944bc652e8536a4d941
vensder <vensder@gmail.com>
2016-02-09 18:52:03 -0500

    lift added

---
+++ lift.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# student: Dmitry Makarov
+
+import os # import of os module
+
+# checking for current version of Python
+import platform
+print('Current version of Python is: ' + platform.python_version() + '\n')
+
+small_lift = 1
+large_lift = 1
+
+start_flour = input('On which flour do you placed?')
+print(start_flour)
