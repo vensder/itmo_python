9d7d6a05121034728f96c80414b3ba257eff3cac
vensder <vensder@gmail.com>
2016-02-10 14:07:25 +0300

    let's cpu works

--- python.py
+++ python.py
@@ -1,8 +1,13 @@
-#!/usr/bin/env python3.4
+#!/usr/bin/env python3.5
 a = 0
 print ('Hello Python!')
 
-for i in range(1,1000000):
-    print(i)
+for i in range(1,100000000):
+#    print(i)
     a = a + 0.1
-    print(a)
+#    print(a)
+
+print("hello")
+print(i)
+print(a)
+        
