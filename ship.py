2da79d1821de9e56cecf2caae6c27960ef1c97c8
vensder <vensder@gmail.com>
2016-02-29 00:36:50 +0300

    closed area of ship marked by 2

--- ship.py
+++ ship.py
@@ -34,18 +34,22 @@ ship3v = [1 for i in range(cells3)]
 ship2v = [1 for i in range(cells2)]
 ship1v = [1 for i in range(cells1)]
 
-def insertv(decks_number):
+def insert_horizontal(decks_number):
     x = randint(0,(9-(decks_number-1)))
     y = randint(0,9)
+    if y > 0:
+        area[y-1][x:x+decks_number] = [2 for i in range(decks_number)]
     area[y][x:x+decks_number] = [1 for i in range(decks_number)]
+    if y < 9:
+        area[y+1][x:x+decks_number] = [2 for i in range(decks_number)]
     print(ship4v)
     print(x)
     print(y)
 
     
 # insert ship4v
-insertv(4)
-insertv(3)
+insert_horizontal(4)
+#insert_horizontal(3)
 #y = randint(0,9)
 #x = randint(0,(9-(cells4-1)))
 #area[y][x:x+cells4] = ship4v
