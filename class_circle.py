ca050d5a14fe734d15e989bbc19cd909b5660191
vensder <vensder@gmail.com>
2016-03-14 14:20:46 +0300

    modules, classes

---
+++ class_circle.py
@@ -0,0 +1,30 @@
+import math
+
+
+class Circle:
+    def __init__(self, x, y, r):
+        self.x = x
+        self.y = y
+        self.r = r
+        
+    def area(self):
+        return math.pi * self.r ** 2
+    
+    @property    
+    def r(self):
+        return self._r
+    
+    @r.setter
+    def r(self,r):
+        if r < 0:
+            raise ValueError('Radius should be nonnegative')
+        self._r = r
+    
+    
+
+c = Circle(0, 0, 1)
+print(c.x)
+print(c.y)
+
+c.r = 2
+print(c.r)
---
+++ my_module5.py
@@ -0,0 +1,14 @@
+
+def f():
+    pass
+
+class C:
+    pass
+
+x = 1
+
+if __name__ == '__main__':
+    print('Hello from main')
+else:
+    print('Hello from my_module5.py')
+
